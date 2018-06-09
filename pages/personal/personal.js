45b43fb4c123de41f2320db92dde3cb7a3726e10
msf <115085794@qq.com>
2018-06-09 16:27:22 +0800

    “..

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -10,19 +10,44 @@ Page({
                         name:"健康查询",
                         url: "weight/weight"
                     },   
-                ]        
+                    {
+                       img:"../../images/green_tri.png",
+                        name:"健康时讯",
+                        url: ""
+                    }, 
+                    {
+                      img: "../../images/gouwu.png",
+                      name: "周边药店",
+                      url: ""
+                    },
+                    {
+                      img: "../../images/label.png",
+                      name: "周边医院",
+                      url: ""
+                    },      
+                    {
+                      img: "../../images/friends.png",
+                      name: "联系我们",
+                      url: ""
+                    },   
+                    {
+                      img: "../../images/setting.png",
+                      name: "设置",
+                      url: ""
+                    },   
+                    
+                ]   
+                     
             }
         ]
     },
     goPage: function (event) {
-      console.log(event.currentTarget.dataset.log);
+     
       wx.navigateTo({
         url: event.currentTarget.dataset.url
       })
     },
     onLoad: function () {
       
-       
-        
     }
 })
